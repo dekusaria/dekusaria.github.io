6aaa44f8485d4db3f49437e0b502350e8fd7c9ef
dekusaria <67778098+dekusaria@users.noreply.github.com>
2021-09-28 14:41:41 -0500

    Initial commit
    Portfolio website initial commit

---
+++ home.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+})
+
+function openNav(){
+    $('.navbar').css("width", "10em");
+    $('.container').css("margin-left","10em");
+}
+
+function closeNav() {
+    $('.navbar').css("width", "0");
+    $('.container').css("margin-left", "0");
+}
